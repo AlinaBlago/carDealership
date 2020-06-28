7d25aa57c8b6381ae6d3622cc9f49a799d01328a
Alina <alinablago19@gmail.com>
2020-06-28 21:21:29 +0300

    Added field "public" in methods MoodException and ConditionerException.

--- src/main/java/ua/com/nix/dz6/carDealership/exception/ConditionerException.java
+++ src/main/java/ua/com/nix/dz6/carDealership/exception/ConditionerException.java
@@ -1,7 +1,7 @@
 package ua.com.nix.dz6.carDealership.exception;
 
 public class ConditionerException extends RuntimeException {
-     ConditionerException(String msg) {
+    public ConditionerException(String msg) {
          super(msg);
     }
 }
--- src/main/java/ua/com/nix/dz6/carDealership/exception/MoodException.java
+++ src/main/java/ua/com/nix/dz6/carDealership/exception/MoodException.java
@@ -1,7 +1,7 @@
 package ua.com.nix.dz6.carDealership.exception;
 
 public class MoodException extends RuntimeException {
-    MoodException(String msg){
+   public MoodException(String msg){
         super(msg);
     }
 }
