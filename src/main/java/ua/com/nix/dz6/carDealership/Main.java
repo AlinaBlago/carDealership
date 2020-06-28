1b03317d6c8577bb979c86deda33af3d6b36ebf6
Alina <alinablago19@gmail.com>
2020-06-28 20:24:38 +0300

    Created project, are listed  duties of director of car dealership in one class.

--- src/main/java/ua/com/nix/dz6/carDealership/Main.java
+++ src/main/java/ua/com/nix/dz6/carDealership/Main.java
@@ -2,7 +2,7 @@ package ua.com.nix.dz6.carDealership;
 
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("Main.main");
+
         DirectorOfCarDealership director = new DirectorOfCarDealership();
         director.startWorkday(new WorkersOfCarDealership());
     }
