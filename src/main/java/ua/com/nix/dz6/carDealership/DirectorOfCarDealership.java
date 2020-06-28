64010d04bc2109072431b4f0ba5c9fffe28f7f39
Alina <alinablago19@gmail.com>
2020-06-28 20:34:22 +0300

    Duties are separated.

--- src/main/java/ua/com/nix/dz6/carDealership/DirectorOfCarDealership.java
+++ src/main/java/ua/com/nix/dz6/carDealership/DirectorOfCarDealership.java
@@ -3,11 +3,18 @@ package ua.com.nix.dz6.carDealership;
 public class DirectorOfCarDealership {
 
     public void startWorkday(WorkersOfCarDealership workers){
-        System.out.println("Открывает здание.");
+        System.out.println("Настроение есть!");
+        System.out.println("Кондиционер включен!");
+        System.out.println("Кофе-машина работает!");
+        work(workers);
         System.out.println("Проводит утренний бриффинг.");
-        System.out.println("Подписывает документы.");
         System.out.println("Дает советы по работе.");
-        System.out.println("Контролирует работу сотрудников.");
         System.out.println("Подводит итоги дня.");
+    }
+
+    public void work(WorkersOfCarDealership workers){
+        System.out.println("Открывает здание.");
+        System.out.println("Подписывает документы.");
+        System.out.println("Контролирует работу сотрудников.");
     }
 }
