493077304d8608507206d36a584759d6db483ffa
Alina <alinablago19@gmail.com>
2020-06-29 19:24:14 +0300

    Added new classes WorkEnvironmentService and WorkEnvironmentServiceImpl

---
+++ src/main/java/ua/com/nix/dz6/carDealership/service/impl/WorkEnvironmentServiceImpl.java
@@ -0,0 +1,13 @@
+package ua.com.nix.dz6.carDealership.service.impl;
+
+import ua.com.nix.dz6.carDealership.service.WorkEnvironmentService;
+
+public class WorkEnvironmentServiceImpl implements WorkEnvironmentService {
+
+    private String
+
+    @Override
+    public void check() {
+
+    }
+}
