e7d107382d26b7e427bff7cd473bad40a66d6f75
Alina <alinablago19@gmail.com>
2020-06-29 20:40:57 +0300

    Modified some classes

--- src/main/java/ua/com/nix/dz6/carDealership/config/ObjectFactory.java
+++ src/main/java/ua/com/nix/dz6/carDealership/config/ObjectFactory.java
@@ -1,9 +1,11 @@
 package ua.com.nix.dz6.carDealership.config;
+import ua.com.nix.dz6.carDealership.annotation.Value;
 import ua.com.nix.dz6.carDealership.config.impl.JavaApplicationConfiguration;
 import ua.com.nix.dz6.carDealership.service.ConditionerService;
 import ua.com.nix.dz6.carDealership.service.impl.WorkProcessServiceImpl;
 import ua.com.nix.dz6.carDealership.util.ResourceUtil;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
@@ -31,12 +33,10 @@ public class ObjectFactory {
         if (type.isInterface()){
             implClass = config.getCurrentImplementation(type);
         }
-        T t;
+        T t = null;
         try {
             t = implClass.getDeclaredConstructor().newInstance();
-
-
-
+            initFields(implClass, t);
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             throw new RuntimeException("невозможно создать класс: " + e.getClass().getName() + " " + ",msg: " + e.getMessage());
         }
@@ -44,8 +44,20 @@ public class ObjectFactory {
         return t;
     }
 
-    private <T> void initFields(Class<T> type, T t){
+    private <T> void initFields(Class<? extends T> implClass, T t){
         Map<String, String> map = ResourceUtil.getResource("application.properties");
+        for (Field field : implClass.getDeclaredFields()){
+            Value annotation = field.getAnnotation(Value.class);
+            if (annotation != null){
+                String value = annotation.value().isEmpty() ? map.get(field.getName()) : annotation.value();
+                field.setAccessible(true);
+                try {
+                    field.set(t, value);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Возникли проблемы с инициализацией филда: " + e.getMessage());
+                }
+            }
+        }
     }
 }
 
--- src/main/java/ua/com/nix/dz6/carDealership/service/impl/WorkEnvironmentServiceImpl.java
+++ src/main/java/ua/com/nix/dz6/carDealership/service/impl/WorkEnvironmentServiceImpl.java
@@ -5,10 +5,10 @@ import ua.com.nix.dz6.carDealership.service.WorkEnvironmentService;
 public class WorkEnvironmentServiceImpl implements WorkEnvironmentService {
 
     @Value
-    private String coffeeMachine;
+    private String coffee;
 
     @Override
     public void check() {
-        System.out.println("Свет есть, " + coffeeMachine + " работает");
+        System.out.println("Кофе машина включена, " + coffee + " варится.");
     }
 }
